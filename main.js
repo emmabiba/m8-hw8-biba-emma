fd88f51f8124318c15fd6e9b35d401024d69a573
Emma Biba <emmabiba@ufl.edu>
2020-10-22 19:32:10 -0500

    Add css style, transition & js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // It is always helpful to use comments in your code!
+var dropdowns = document.getElementsByClassName('dropdown');
+// console.log(dropdowns);
+
+var navLinks = document.getElementsByClassName('nav-link');
+// console.log(navLinks);
+
+function toggleDropdown() {
+    // for( i = 0; i < dropdowns.length; i++ ){
+    //     dropdowns[i].classList.toggle('show');
+    //}
+
+    this.children[1].classList.toggle('show');
+}
+
+for( i = 0; i < navLinks.length; i ++){
+    navLinks[i].addEventListener("mouseenter", toggleDropdown)
+    navLinks[i].addEventListener("mouseleave", toggleDropdown)
+}
